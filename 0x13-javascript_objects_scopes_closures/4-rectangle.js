ef4d26f1b063184b1442c984b34e891e13163287
Possible-generation <ajigbotoshojoshua2000@.com>
2023-04-13 01:57:54 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -6,19 +8,16 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-
   rotate () {
     let temp = this.height;
     this.height = this.width;
     this.width = temp;
   }
-
   double () {
     this.width *= 2;
     this.height *= 2;
